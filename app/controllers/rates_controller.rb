025d5309e5dc988ab090352281a8836a08ab0bbd
Hamish Lees <hamishlees6@gmail.com>
2019-10-09 19:36:35 +1100

    Add in Fixer credentials. Adjust the way errors are displayed

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,33 +1,28 @@
 class RatesController < ApplicationController
-    # GET /
-    def index
-      @rates = Rate.all
-      @rate = Rate.new
-    end
-  
-    # POST /
-    def create
-      rate_params = params.require(:rate).permit(:currency_1, :currency_2)
-  
-      @rate = Rate.new(rate_params)
-      if @rate.save
-        redirect_to rates_path
-      else
-        @error = @rate.errors.messages[:currency_1][0]
+  # GET /
+  def index
+    @rates = Rate.all
+    @rate = Rate.new
+  end
 
-        # Need another DB call to get all rates on render
-        @rates = Rate.all
-        render 'index'
-      end
-    end
-  
-    # DELETE /:id
-    def destroy
-      Rate.find(params[:id]).destroy
-  
-      redirect_to rates_url
+  # POST /
+  def create
+    rate_params = params.require(:rate).permit(:currency_1, :currency_2)
+
+    @rate = Rate.new(rate_params)
+    if @rate.save
+      redirect_to rates_path
+    else
+      # Need another DB call to get all rates on render
+      @rates = Rate.all
+      render 'index'
     end
   end
-  
-  
-  
+
+  # DELETE /:id
+  def destroy
+    Rate.find(params[:id]).destroy
+
+    redirect_to rates_url
+  end
+end
