1fbb4d724677c81307c77666a7a663979fd6e57c
Hamish Lees <hamishlees6@gmail.com>
2019-10-10 23:48:54 +1100

    Add in route, controller and view for management page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/', to: 'rates#index', as: 'rates'
   post '/', to: 'rates#create'
   delete '/:id', to: 'rates#destroy', as: 'destroy_rate'
+
+  get '/management', to: 'management#index', as: 'management'
 end
