3d03ece67a39162bfe95e6b986bfd4eab429da6c
Hamish Lees <hamishlees6@gmail.com>
2019-10-11 14:24:59 +1100

    Adjust application helper so rates are checked on every render

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    # If the Rate has not been updated in the last 60 mins, update them before the app begins
+
+    RateWorker.update_rates(nil) if Time.now - Rate.first.updated_at > (60 * 60) # 60 seconds * 60 minutes
 end
